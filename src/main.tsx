c630255d17bf2c4583be36cf1cf9f6c7e4a18a12
morefa1986-jpg <morefa1986@gmail.com>
2026-08-24 06:54:04 +0330

    Install fail-closed LAN conflict guard

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { installStateConflictGuard } from './services/stateConflictGuard';
+
+installStateConflictGuard();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
